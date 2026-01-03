0bbc99ca7324297afc66c94be2bc1691ebf2d2f8
cypher.nue.dom.my.id <cypher@nue.domcloud.co>
2026-01-03 07:53:15 +0000

    removed

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,2 @@
+# We expect you export app variable in app/__init__.py or app.py!
 from app import app as application
-import logging
-from logging.handlers import RotatingFileHandler
-import os
-
-LOG_FILE = os.path.expanduser('~/logs/flask.log')
-
-handler = RotatingFileHandler(
-    LOG_FILE,
-    maxBytes=5 * 1024 * 1024,
-    backupCount=3
-)
-
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter(
-    '%(asctime)s - %(levelname)s - %(message)s'
-)
-handler.setFormatter(formatter)
-
-application.logger.addHandler(handler)
-application.logger.setLevel(logging.INFO)
-
-application.logger.info("Flask logging started")
